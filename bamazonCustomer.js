77c4629029bd044afac2be109220e0275efee132
UnicornBear <jtbregenzer@gmail.com>
2019-06-08 20:21:06 -0600

    update logic to both - made Manager list option - require cli-table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require('cli-table');
 
 //  create the connection to mySQL database
 var connection = mysql.createConnection({
@@ -15,69 +16,170 @@ var connection = mysql.createConnection({
 // connect to mySQL 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("Connection Successful");
-    makeTable();
+    // console log to check if successful connection
+    // console.log("Connection Successful");
 })
 
+console.log("Welcome Customer, to Bamazon!")
+display();
+
+
 // create function to display table for bamazon products 
-var makeTable = function(){
-    connection.query("SELECT * FROM products", function(err,res){
-        for(var i=0; i<res.length; i++){
-            console.log(res[i].item_id+" | "+res[i].product_name+" | "+
-            res[i].department_name+" | "+res[i].price+" | "+
-            res[i].stock_quantity+"\n");
+function display(){
+    connection.query("SELECT * FROM products", 
+    function (err, res) {
+        if (err) throw err;
+        var table = new Table({
+            head: ["ID", "Product Name", "Department", "Price", "Stock Qty"],
+            colWidths: [6, 45, 16, 11, 11]
+        });
+        
+        for (var i = 0; i < res.length; i++) {
+            table.push(
+                [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity],
+            );
         }
-    askCustomer(res);
+        console.log(table.toString());
+        start();
     })
 }
 
-// create function to interact with the customer
-var askCustomer = function(res){
-    inquirer.prompt([{
-        type:'input',
-        name:'choice',
-        message:"Hello, what would you like to purchase?"
-    }]).then(function(answer){
-        var correct = false;
-        //if customer chooses 'Q' - then exit app process
-        if(answer.choice.toUpperCase()=="Q"){
-            process.exit();
-        }
-        for(var i=0;i<res.length;i++){
-            if(res[i].product_name==answer.choice){
-                correct=true;
-                var product=answer.choice;
-                var id=i;
-                inquirer.prompt({
-                    type:"input",
-                    name:"quant",
-                    message:"How many would you like to purchase?",
-                    validate: function(value){
-                        if(isNaN(value)==false){
-                            return true;
-                        } else {
-                            return false;
-                        }
-                    }
-                }).then(function(answer){   
-                    if((res[id].stock_quantity-answer.quant)>0){
-                        connection.query("UPDATE products SET stock_quantity='"+
-                        (res[id].stock_quantity-answer.quant)+
-                        "' WHERE product_name='"+product+"'", 
-                    function(err,res2){
-                        console.log("You Purchased the Product");
-                        makeTable();
-                        })
-                    } else {
-                        console.log("Not a Valid Purhcase!");
-                        askCustomer(res);
-                    }
-                })
+
+//function to start interaction with Customer
+function start() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "itemID",
+            message: "What is the ID of the item you would like to purchase? [Quit with Q]",
+            validate: function(inputID) {
+                //if users enter q or Q, exit the program.
+                if (inputID.toLowerCase() === "q") {
+                    connection.end();
+                    process.exit();
+                }
+                //users need to enter a number between 1 to 10 because there are 10 items in the table.
+                else if ((!isNaN(inputID)) && (inputID<=10) && (inputID>0)) {
+                    return true;
+                }
+                console.log("Please enter a valid ID.");
+                return false;
+            }
+        },
+        {
+            type: "input",
+            name: "quantity",
+            message: "How many would you like? [Quit with Q]",
+            validate: function(inputQ) {
+                //if users enter q or Q, exit the program.
+                if (inputQ.toLowerCase() === "q") {
+                    connection.end();
+                    process.exit();
+                }
+                //users need to enter a number
+                else if (!isNaN(inputQ)) {
+                    return true;
+                }
+                console.log("Please enter a valid quantity.");
+                return false;
             }
         }
-        if(i==res.length && correct==false){
-            console.log("This is not a valid selection");
-            askCustomer(res);
-        }
-    })
+    ]).then(function (answer) {
+
+        connection.query("SELECT * FROM products", function(err, res) {
+            if (err) throw err;
+            //query through out the table to find the matched ID as the selected item.
+            var chosenItem ="";
+              for (var i = 0; i < res.length; i++) {
+                if (res[i].item_id === parseInt(answer.itemID)) {
+                  chosenItem = res[i];
+                }
+              }
+              
+              //check if the inventory has enough of the product to meet the customer's request.
+              //if it has enough of the product, update the SQL database to reflect the remaining quantity
+              if (chosenItem.stock_quantity >= parseInt(answer.quantity)) {
+                connection.query(
+                  "UPDATE products SET ? WHERE ?",
+                  [
+                    {
+                      stock_quantity: chosenItem.stock_quantity-= parseInt(answer.quantity)
+                    },
+                    {
+                      item_id: chosenItem.item_id
+                    }
+                  ],
+                  function(error) {
+                    if (error) throw err;
+                    //Once the update goes through, show the customer the total cost of their purchase.
+                    var totalCost = answer.quantity * chosenItem.price;
+                    console.log("Thank you. Successfully purchased "+ answer.quantity + " " + chosenItem.product_name + ".");
+                    console.log("Your total cost is $"+ totalCost + ".");
+                    display();
+                  }
+                );
+              }
+              else {
+                //show error message when inventory is not sufficient to prevent the order from going through.
+                console.log("Sorry. Insufficient quantity! Please select another item or change your quantity.");
+                start();
+              }
+            });
+    });
+
 }
+
+
+
+//OLD QUERY 
+
+// create function to interact with the customer
+// function askCustomer(res){
+//     inquirer.prompt([{
+//         type:'input',
+//         name:'choice',
+//         message:"Hello, what would you like to purchase?"
+//     }]).then(function(answer){
+//         var correct = false;
+//         //if customer chooses 'Q' - then exit app process
+//         if(answer.choice.toUpperCase()=="Q"){
+//             process.exit();
+//         }
+//         for(var i=0;i<res.length;i++){
+//             if(res[i].product_name==answer.choice){
+//                 correct=true;
+//                 var product=answer.choice;
+//                 var id=i;
+//                 inquirer.prompt({
+//                     type:"input",
+//                     name:"quant",
+//                     message:"How many would you like to purchase?",
+//                     validate: function(value){
+//                         if(isNaN(value)==false){
+//                             return true;
+//                         } else {
+//                             return false;
+//                         }
+//                     }
+//                 }).then(function(answer){   
+//                     if((res[id].stock_quantity-answer.quant)>0){
+//                         connection.query("UPDATE products SET stock_quantity='"+
+//                         (res[id].stock_quantity-answer.quant)+
+//                         "' WHERE product_name='"+product+"'", 
+//                     function(err,res2){
+//                         console.log("You Purchased the Product");
+//                         makeTable();
+//                         })
+//                     } else {
+//                         console.log("Not a Valid Purhcase!");
+//                         askCustomer(res);
+//                     }
+//                 })
+//             }
+//         }
+//         if(i==res.length && correct==false){
+//             console.log("This is not a valid selection");
+//             askCustomer(res);
+//         }
+//     })
+// }
--- bamazonManager.js
+++ bamazonManager.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require('cli-table');
 
 //  create the connection to mySQL database
 var connection = mysql.createConnection({
@@ -12,95 +13,232 @@ var connection = mysql.createConnection({
     database:"bamazon"
 })
 
-// connect to mySQL 
-// connection.connect(function(err) {
-//     if (err) throw err;
-//     console.log("Connection Successful");
-//     makeTable();
-// })
+connection.connect(function (err) {
+    if (err) throw err;
+});
+
+console.log("Welcome to Bamazon!");
+start();
 
 
-// create function to display table for bamazon products 
-var makeTable = function(){
-    connection.query("SELECT * FROM products", function(err,res){
-        if(err) throw err;
-        console.log("item_id\tproduct_name\tdepartment name\tprice\tnumber in stock");
-        console.log("-------------------------------------------");
-        for(var i=0; i<res.length; i++){
-            console.log(res[i].item_id+"\t"+res[i].product_name+"\t"+
-            res[i].department_name+"\t"+res[i].price+"\t"+
-            res[i].stock_quantity);
+//function at the beginning to prompt questions.
+function start() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "task",
+            message: "What would you like to do?",
+            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "Quit"]
         }
-    console.log("-----------------------------");
-    askManager();
-    })
+    ]).then(function (answer) {
+        //use switch/case for different functions to run based on different answers.
+        switch (answer.task) {
+        
+            case "View Products for Sale":
+                display();
+                break;
+
+            case "View Low Inventory":
+                viewLowInv();
+                break;
+
+            case "Add to Inventory":
+                addInv();
+                break;
+
+            case "Add New Product":
+                addProduct();
+                break;
+
+            case "Quit":
+                quitfun();
+                break;                
+
+        }
+
+    });
 }
 
-// create function to ask Manager what they want to do
-var askManager = function(res) {
-    inquirer.prompt([{
-        type:"rawlist",
-        name:"choice",
-        message:"What would you like to do",
-        choices:["add new item","add quantity"]
-    }]).then(function(val){
-        //choice - add new item
-        if(val.choice=='add new item'){
-            addItem();
-        }
-        //choice - add quantity
-        if(val.choice=="add quantity"){
-            addQuatity(res);
-        }
-    })
-}
 
-// create function to add Item to bamazon.products (product_name)
-function addItem(){
-    inquirer.prompt([{
-        type:"input",
-        name:"productname",
-        message:"What is product name you would like to add?"
-    },{
-        type:"input",
-        name:"departmentname",
-        message:"What department?"
-    },{
-        type:"input",
-        name:"price",
-        message:"What is the price?"
-    },{
-        type:"input",
-        name:"stockquantity",
-        message:"How many to add?"
-    }]).then(function(val){
-        connection.query("INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ('"+val.productname+"','"+val.departmentname+"','"+val.price+"','"+val.stockquantity+"');",
-        function(err,res){ 
-            if(err)throw err;
-            console.log(val.productname+" Added to the Bamazon");
-            makeTable();
-        })
-    })
-}
+//function to "View Products for Sale". Select table from Database and display the table using cli-table.
+function display() {
+    connection.query("SELECT * FROM products", 
+    function (err, res) {
+        if (err) throw err;
+        var table = new Table({
+            head: ["ID", "Product Name", "Department", "Price", "Stock Qty"],
+            colWidths: [6, 45, 16, 11, 11]
+        });
+    
+        for (var i = 0; i < res.length; i++) {
+            table.push(
+                [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity],
+            );
+        }
+        console.log(table.toString());
+        start()
+    });
+};
 
-// create funection to add Quantity to a bamazon.product (stock_quantity)
-function addQuantity(res){
-    inquirer.prompt([{
-        type:"input",
-        name:"productname",
-        message:"What product are you adding quantity?"
-    },{
-        type:"input",
-        name:"addquantity",
-        message:"What quantity are you adding?"
-    }]).then(function(val){
-        for(i=0;i<res.length;i++){
-            if(res[i].product_name==val.productname){
-                connection.query("UPDATE products SET stock_quantity=stockquantity+
-                ")
+
+
+//function to View Low Inventory.
+function viewLowInv() {
+    //query database to list items with an inventory count lower than five.
+    var query = "SELECT * FROM products WHERE stock_quantity <= 10";
+    connection.query(query, function(err, res){
+        if (err) throw err;
+
+        //display the table using cli-table.
+        var table = new Table({
+            head: ["ID", "Product Name", "Department", "Price", "Stock Qty"],
+            colWidths: [6, 45, 16, 11, 11]
+        });
+    
+        for (var i = 0; i < res.length; i++) {
+            table.push(
+                [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity],
+            );
+        }
+        console.log(table.toString());
+        start()
+    })
+};
+
+
+//function to Add to Inventory
+function addInv() {
+    //prompt questions to get selected item id and quantity
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "itemID",
+            message: "What is the ID of the item you would like to add more quantity?",
+            //users need to enter number
+            validate: function(inputID) {
+                if (!isNaN(inputID)) {
+                    return true;
+                }
+                console.log("Please enter a valid ID.");
+                return false;
+            }
+        },
+        {
+            type: "input",
+            name: "quantity",
+            message: "How many would you like to add to the inventory?",
+            //users need to enter number
+            validate: function(inputQ) {
+                if (!isNaN(inputQ)) {
+                    return true;
+                }
+                console.log("Please enter a valid quantity.");
+                return false;
             }
         }
-    })
-}
+    ]).then(function (answer) { 
+        
+        connection.query("SELECT * FROM products", function(err, res) {
+            if (err) throw err;
+        
+            //if users enter an item ID outside total items range, then throw error message.
+            if((parseInt(answer.itemID)>res.length) || (parseInt(answer.itemID)<=0)) {
+                console.log("Please enter a valid ID.");
+            }
 
-makeTable();
+            //otherwise, proceed to loop through the data and find matched item id as the selected item
+            var chosenItem ="";
+            for (var i = 0; i < res.length; i++) {
+                if (res[i].item_id === parseInt(answer.itemID)) {
+                chosenItem = res[i];
+                }
+            }
+            
+            //update the quantity for selected item id
+            connection.query("UPDATE products SET ? WHERE ?",
+                [
+                    {
+                    stock_quantity: chosenItem.stock_quantity += parseInt(answer.quantity)
+                    },
+                    {
+                    item_id: chosenItem.item_id
+                    }
+                ],
+                function(error) {
+                    if (error) throw err;
+                    //show message that certain product and its quantity has been updated in the inventory.
+                    console.log("Successfully updated/added "+ answer.quantity + " " + chosenItem.product_name + " to the inventory.");
+                    display();
+                }
+                );
+
+        });
+
+    });
+
+};
+
+
+//function to Add New Product
+function addProduct() {
+    //prompt questions to get product name, department, cost, and quantity for the new product 
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "newProductName",
+            message: "What is the name of the product you would like to add?"
+        },
+        {
+            type: "list",
+            name: "department",
+            message: "Which department does this product fall into?",
+            choices: ["Electronics", "Beauty", "Shoes", "Sports", "Home", "Books"]
+        },
+        {
+            type: "input",
+            name: "cost",
+            message: "How much does it cost?",
+            validate: function(cost) {
+                if (!isNaN(cost)) {
+                    return true;
+                }
+                console.log("Please enter a valid cost.");
+                return false;
+            }
+        },
+        {
+            type: "input",
+            name: "iniQuantity",
+            message: "How many do we have?",
+            validate: function(iniQuantity) {
+                if (!isNaN(iniQuantity)) {
+                    return true;
+                }
+                console.log("Please enter a valid quantity.");
+                return false;
+            }
+        }
+    ]).then(function (answers){
+
+        //grab the new product info from answer and add to (insert into) the database table
+        var queryString = "INSERT INTO products SET ?";
+        connection.query(queryString, {
+            product_name: answers.newProductName,
+            department_name: answers.department,
+            price: answers.cost,
+            stock_quantity: answers.iniQuantity,
+        })
+
+        //show message that the product has been added.
+        console.log(answers.newProductName + " has been added to Bamazon.");
+
+        display();
+    });     
+
+};
+
+
+//function to quit the program
+function quitfun() {
+    connection.end();
+};
